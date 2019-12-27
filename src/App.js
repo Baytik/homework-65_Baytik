40e3eaa62c7cdedabc8b9c1cd8e337b6a3db81a2
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-12-27 18:29:00 +0600

    Закончил

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
+import Layout from "./Components/Layout/Layout";
+import Contents from "./Components/Content/Contents";
+import Edit from "./Components/Edit/Edit";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 class App extends Component{
   render() {
     return(
-        <div className="App">
-
-        </div>
+        <BrowserRouter>
+        <Layout/>
+        <Switch>
+          <Route path="/pages/admin" component={Edit}/>
+          <Route path="/pages/:name" component={Contents}/>
+        </Switch>
+        </BrowserRouter>
     )
   }
 }
